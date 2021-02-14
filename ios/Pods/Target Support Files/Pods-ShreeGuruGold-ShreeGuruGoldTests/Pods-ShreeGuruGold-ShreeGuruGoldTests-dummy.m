c42bec55c3f7b92c6f18ec86a703402ab6f7225b
Ajinkya03 <gt20.ajinkya@gmail.com>
2021-02-14 13:31:54 +0530

    update one

---
+++ ios/Pods/Target Support Files/Pods-ShreeGuruGold-ShreeGuruGoldTests/Pods-ShreeGuruGold-ShreeGuruGoldTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ShreeGuruGold_ShreeGuruGoldTests : NSObject
+@end
+@implementation PodsDummy_Pods_ShreeGuruGold_ShreeGuruGoldTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-ShreeGuruGold/Pods-ShreeGuruGold-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ShreeGuruGold : NSObject
+@end
+@implementation PodsDummy_Pods_ShreeGuruGold
+@end
